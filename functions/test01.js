f2467a42ce64ed22d38e53bea11ff7a22bf22caa
sesneaky <se@sesneaky.com>
2019-02-14 20:50:47 +0100

    Adds XML response (partly)

--- functions/test01.js
+++ functions/test01.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: '<?xml version="1.0" encoding="utf-8"?><MESSAGE message="Thanks for registering our product!"><KEY>I4qtvzcihwD8NvDK2cOnH7hMf26lQX2HmW9RrAG3</KEY></MESSAGE>'
     });
 }
